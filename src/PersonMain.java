64887eb540d729ef09e7e1fa98ac824712f04b39
andreas nehl <andenk@hotmail.se>
2021-01-22 13:34:15 +0100

    while

--- src/PersonMain.java
+++ src/PersonMain.java
@@ -2,29 +2,50 @@ import java.util.Scanner;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class PersonMain {
+    static PersonDAO pdao = new PersonDAOWithJPAImpl();
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-
-        boolean flagg=true;
-        System.out.println("tal");
-        Scanner sc = new Scanner(System.in);
-        int input;
-do {
-    input = sc.nextInt();
-    switch (input) {
-        case 1:
-    }
-}while(flagg=true);
-	PersonDAO pdao = new PersonDAOWithJPAImpl();
 
         System.out.println("------------------- LETs Start --------------------------");
         System.out.println("---------------------------------------------------------");
-        Person ppp = new Person("ID","user", "password", "firstName", "lastName","email","phone");
 
+        boolean flagg=true;
+        int input;
+do {
+    System.out.println(" Press " +
+            " 1 create "+ "\n" +" 2 select * "+ "\n" +" 3 Update "+ "\n" +" 4 Delete"+ "\n" +" 5 Exit" );
+    input = sc.nextInt();
+    switch (input) {
+        case 1:
+            createMain(pdao);
+            break;
+        case 2:
+            // select *
 
-        pdao.create(ppp);
+            break;
+        case 3:
+            // update
+            break;
+        case 4:
+            // delete
+            break;
+        case 5:
+            System.exit(0);
+            break;
+        case 6:
+            searchForUser(pdao);
+            break;
+    }
+}while(flagg=true);
 
+    }
 
+    private static void searchForUser() {
         System.out.println(pdao.getByUserName("SiHa"));
+    }
 
+    private static void createMain() {
+        Person ppp = new Person("ID","user", "password", "firstName", "lastName","email","phone");
+        pdao.create(ppp);
     }
 }
